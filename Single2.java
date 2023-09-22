91347ed5c26b237ec5faf23b4d7c0384ae293685
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-09-22 11:29:52 +0400

    feat: comment on single2

--- Single2.java
+++ Single2.java
@@ -3,6 +3,8 @@ public class Single2 {
     public static Integer singleNumber(int[] nums){
         for(int i = 0; i <= nums.length; i++){
             for(int j = 0; j <= nums.length; j++){
+                // iterate over nums arrays and if the number is same and is not placed at the same index break loop(its not single)
+                // otherwise return the number since its single
                 if(i != j && nums[i] == nums[j]){
                     break;
                 }else{
